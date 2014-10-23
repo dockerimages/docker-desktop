1dd073047971b7caeb0624cb2e994078e66b8ebc
Frank Lemanschik <frank@github.dspeed.eu>
2014-10-23 19:13:14 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 # Create the directory needed to run the sshd daemon
 mkdir /var/run/sshd 
+mkdir /home/docker/.ssh
 
 # Add docker user and generate a random password with 12 characters that includes at least one capital letter and number.
 DOCKER_PASSWORD=`pwgen -c -n -1 12`
